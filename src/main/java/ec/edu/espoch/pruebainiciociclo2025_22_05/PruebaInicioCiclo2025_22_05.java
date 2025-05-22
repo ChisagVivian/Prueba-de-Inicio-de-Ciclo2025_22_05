b04c1b646d90dbce4bf5dc0f74f66666a9791386
unknown <ChisagVivian>
2025-05-22 18:24:26 -0500

    Prueba-de-Inicio-de-Ciclo2025_22_05

---
+++ src/main/java/ec/edu/espoch/pruebainiciociclo2025_22_05/PruebaInicioCiclo2025_22_05.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espoch.pruebainiciociclo2025_22_05;
+
+/**
+ *
+ * @author Usuario
+ */
+public class PruebaInicioCiclo2025_22_05 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
